2f7d3fbd93430efb31cd22319f7996a7ce6865b6
babynabeauty <1130334236@qq.com>
2021-12-11 11:14:05 +0800

    修改文件上传代码

---
+++ untitled/src/main/java/cqu/mvc/model/domain/FileEntity.java
@@ -0,0 +1,50 @@
+package cqu.mvc.model.domain;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.File;
+
+/**
+ * 定义文件类
+ */
+@Entity
+public class FileEntity {
+
+    @Id
+    @Column(length=50,nullable=false)
+    private String id;
+
+    @Column(length=50,nullable=false)
+    private String name;
+
+    private byte[] fileByte;
+    public FileEntity(){
+    }
+
+    public FileEntity(String id,String name) {
+        super();
+        this.id=id;
+        this.name=name;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public byte[] getFileByte() {
+        return fileByte;
+    }
+    public void setFileByte(byte[] fileByte) {
+        this.fileByte = fileByte;
+    }
+
+}
